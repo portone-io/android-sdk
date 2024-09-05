383a8bb02486e71f64681048046a113880a5801d
youjmen <youjmen15@gmail.com>
2024-09-05 16:30:36 +0900

    feat: request issue billing key sample page

---
+++ app/src/main/java/io/portone/portonesdk/IssueBillingKeyTestActivity.kt
@@ -0,0 +1,57 @@
+package io.portone.portonesdk
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import io.portone.portonesdk.databinding.ActivityIssueBillingKeyTestBinding
+import io.portone.sdk.IssueBillingKeyCallback
+import io.portone.sdk.IssueBillingKeyRequest
+import io.portone.sdk.IssueBillingKeyResponse
+import io.portone.sdk.PaymentCallback
+import io.portone.sdk.PaymentRequest
+import io.portone.sdk.PaymentResponse
+import io.portone.sdk.PortOne
+import kotlinx.serialization.json.Json
+
+class IssueBillingKeyTestActivity : BaseActivity<ActivityIssueBillingKeyTestBinding>() {
+    private val issueBillingKeyActivityResultLauncher =
+        PortOne.registerForIssueBillingKeyActivity(this, callback = object : IssueBillingKeyCallback {
+            override fun onSuccess(response: IssueBillingKeyResponse.Success) {
+                AlertDialog.Builder(this@IssueBillingKeyTestActivity)
+                    .setTitle("빌링키 발급 성공")
+                    .setMessage(response.toString())
+                    .show()
+            }
+
+            override fun onFail(response: IssueBillingKeyResponse.Fail) {
+                AlertDialog.Builder(this@IssueBillingKeyTestActivity)
+                    .setTitle("빌링키 발급 실패")
+                    .setMessage(response.toString())
+                    .show()
+            }
+
+        })
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnIssueBillingKey.setOnClickListener {
+            try {
+                PortOne.requestIssueBillingKey(
+                    this,
+                    request = Json.decodeFromString<IssueBillingKeyRequest>(binding.etIssueBillingKeyJson.text.toString()),
+                    resultLauncher = issueBillingKeyActivityResultLauncher
+                )
+            } catch (e: Exception) {
+                println(e.toString())
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun setViewBinding(inflater: LayoutInflater): ActivityIssueBillingKeyTestBinding {
+        return ActivityIssueBillingKeyTestBinding.inflate(inflater)
+    }
+}

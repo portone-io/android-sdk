ab9ea0f57c2bfba38c2a78c58656233d042614eb
CirnoV <cirnov59@gmail.com>
2025-07-28 06:54:03 +0900

    기존 타입들 제거

--- sdk/src/main/java/io/portone/sdk/android/request/type/billingkey/IssueBillingKeyMethod.kt
+++
@@ -1,8 +0,0 @@
-package io.portone.sdk.android.request.type.billingkey
-
-internal enum class IssueBillingKeyMethod {
-    CARD,
-    MOBILE,
-    EASY_PAY,
-    PAYPAL,
-}
--- sdk/src/main/java/io/portone/sdk/android/type/GiftCertificateType.kt
+++
@@ -1,8 +0,0 @@
-package io.portone.sdk.android.type
-
-enum class GiftCertificateType {
-    BOOKNLIFE,
-    SMART_MUNSANG,
-    CULTURELAND,
-    CULTURE_GIFT;
-}
